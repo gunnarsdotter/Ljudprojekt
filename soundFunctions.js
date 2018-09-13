6fca970a80b3895e4c8c4c0e4dfab5e465b61311
Petra <gunnarsdotter@live.com>
2018-09-13 18:26:42 +0200

    Just having fun but now im going to eat. bye bye

--- soundFunctions.js
+++ soundFunctions.js
@@ -1,4 +1,4 @@
-var promise;
+var audioInput;
 
 function printHej() {
     window.alert("Hej");
@@ -6,11 +6,22 @@ function printHej() {
 
 function startAudio() {
     try {
-        promise = navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+        audioInput = navigator.mediaDevices.getUserMedia({ audio: true, video: false });
     } catch (err) {
         window.alert("Det gick inte hitta ljudkällan.");
     }   
 }
 function stopAudio() {
     
+}
+function isActiveAudio() {
+    window.alert(audioInput.active());
+}
+
+function nobody() {
+    //create audio context
+    //create source
+    //create effects nodes
+    //Finle destenatin
+    //connect soours up to effects and the effekt to destination
 }
